7105471a2d23b51920ffe91fb7d6fecd8c6d1eb6
NinaAndreeaPopovici <ninandreeapopovici@gmail.com>
2020-02-10 20:20:51 +0100

    Commit x

---
+++ typewriter_starter_files/typewriter.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let typeWriter = document.getElementById("type-writer");
+let dataText = typeWriter.getAttribute("data-text");
+console.log(dataText);
+let count = 0;
+let dataTextLength = dataText.length;
+
+let setText = () => {
+  setTimeout(() => {
+    typeWriter.textContent += dataText.charAt(count);
+    count++;
+    if (count <= dataTextLength) {
+      setText();
+    }
+  }, 100);
+};
+
+window.onload = () => {
+  setText();
+};
